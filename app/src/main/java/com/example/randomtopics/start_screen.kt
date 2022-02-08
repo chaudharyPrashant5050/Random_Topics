5c3b37aef3e2e4283765698a0dae98f748a38893
X <chaudharyPrashant5050@gmail.com>
2022-02-08 21:40:53 +0530

    added splash screen && start screen layout V 1.0

---
+++ app/src/main/java/com/example/randomtopics/start_screen.kt
@@ -0,0 +1,11 @@
+package com.example.randomtopics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class start_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_screen)
+    }
+}
